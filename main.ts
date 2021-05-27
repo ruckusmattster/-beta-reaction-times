c2f5584a0e295f51c90bbeef23d30889c276b827
ruckusmattster <77196088+ruckusmattster@users.noreply.github.com>
2021-05-27 13:48:18 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,75 @@
+let running = false
+let end = 0
+let start = 0
+let false_start = false
+input.onButtonPressed(Button.A, function () {
+    if (running) {
+        running = false
+        end = input.runningTime()
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            # # . . .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showNumber(end - start)
+    } else {
+        false_start = true
+        basic.showLeds(`
+            . . . . .
+            # . # . .
+            . # . . .
+            # . # . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    running = false
+    false_start = false
+    basic.pause(1000 + randint(0, 2000))
+    if (!(false_start)) {
+        start = input.runningTime()
+        running = true
+        led.stopAnimation()
+        basic.clearScreen()
+        led.plot(randint(0, 4), randint(0, 4))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (running) {
+        running = false
+        end = input.runningTime()
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            `)
+        basic.pause(1000)
+        basic.showNumber(end - start)
+    } else {
+        false_start = true
+        basic.showLeds(`
+            . . . . .
+            . . # . #
+            . . . # .
+            . . # . #
+            . . . . .
+            `)
+    }
+})
